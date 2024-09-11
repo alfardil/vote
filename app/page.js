05a8adaea6a45b2a8eb88d952c97fc33974f109d
Alfardil Alam <fardilfayed@gmail.com>
2024-09-11 19:01:17 -0400

    init commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,86 @@
+"use client";
+
 import Image from "next/image";
-import styles from "./page.module.css";
+import { Box, Container, Button } from "@mui/material";
+import * as React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { blue, red } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: blue[300],
+      main: blue[500],
+      dark: blue[700],
+      darker: blue[900],
+    },
+    secondary: {
+      light: red[300],
+      main: red[500],
+      dark: red[700],
+    },
+  },
+});
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <ThemeProvider theme={theme}>
+      <Container
+        disableGutters
+        maxWidth={false}
+        sx={{
+          display: "flex",
+          height: "100vh",
+          mt: 4,
+        }}
+      >
+        <Box
+          sx={{
+            width: "50%", // Left half
+            backgroundColor: theme.palette.secondary.light, // Light red background
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            src="/images/trump.avif"
+            alt="Donald Trump"
+            width={300}
+            height={300}
+            style={{ objectFit: "cover" }}
+          />
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <Button variant="contained" color="secondary" sx={{ mt: 2 }}>
+            {" "}
+            Donald Trump
+          </Button>
+        </Box>
+
+        <Box
+          sx={{
+            width: "50%", // Right half
+            backgroundColor: theme.palette.primary.light, // Light blue background
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
           <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/images/kamala.webp"
+            alt="Kamala"
+            width={300} // Adjust image size
+            height={300}
+            style={{ objectFit: "cover" }} // Ensure image covers full width
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+            {" "}
+            Kamala Harris{" "}
+          </Button>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
